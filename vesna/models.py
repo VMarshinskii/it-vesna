ebd32d50be4dbdf4ecd90e64687eedecac1b8539
Дмитрий Баранов <demon.asgard@gmail.com>
2014-12-01 13:40:51 +0500

    Добавлена модель заявки.

--- vesna/models.py
+++ vesna/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+
+    author = models.ForeignKey(User)
+
+    title = models.CharField(max_length=5000)
